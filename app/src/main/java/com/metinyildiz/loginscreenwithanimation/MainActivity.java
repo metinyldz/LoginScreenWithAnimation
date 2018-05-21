d84301fe6988006de841e219621140b7a0c6348b
Metin <metinyildiz2006@gmail.com>
2018-05-21 20:17:15 +0300

    Initial commit

---
+++ app/src/main/java/com/metinyildiz/loginscreenwithanimation/MainActivity.java
@@ -0,0 +1,33 @@
+package com.metinyildiz.loginscreenwithanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    android.os.Handler handler = new android.os.Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable,2000);
+
+    }
+}
